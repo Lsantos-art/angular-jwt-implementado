2b7e6f38a4ced2bcab512922a1697d96ba663d60
Lindomar <lsantosmanga21@gmail.com>
2021-07-21 20:11:20 -0300

    Angular Material adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
@@ -11,20 +12,26 @@ import { appRoutingModule } from './app.routing';
 
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from './home';
-import { LoginComponent } from './login';
+import { LoginComponent } from './login';;
+import { ContatosComponent } from './home/contatos/contatos.component'
+import { MatPaginatorModule, MatTableModule } from '@angular/material';
 
 @NgModule({
     imports: [
         BrowserModule,
         ReactiveFormsModule,
         HttpClientModule,
-        appRoutingModule
+        appRoutingModule,
+        MatTableModule,
+        MatPaginatorModule,
+        BrowserAnimationsModule
     ],
     declarations: [
         AppComponent,
         HomeComponent,
-        LoginComponent
-    ],
+        LoginComponent,
+        ContatosComponent
+      ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
